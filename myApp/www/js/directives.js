7be5c8b1b1e563e37ffb82cc402211c13c812e07
shah-vivek69 <shah.vivek69@gmail.com>
2017-03-21 20:57:53 +0800

    Form validation added to the Signup and login page

--- myApp/www/js/directives.js
+++ myApp/www/js/directives.js
@@ -150,6 +150,24 @@ angular.module('mmc.directives', [])
                     });
                 }
             };
+    })
+    .directive("compareTo", function() {
+        return {
+            require: "ngModel",
+            scope: {
+                otherModelValue: "=compareTo"
+            },
+            link: function(scope, element, attributes, ngModel) {
+                
+                ngModel.$validators.compareTo = function(modelValue) {
+                    return modelValue == scope.otherModelValue;
+                };
+    
+                scope.$watch("otherModelValue", function() {
+                    ngModel.$validate();
+                });
+            }
+        };
     });
 
 ;
